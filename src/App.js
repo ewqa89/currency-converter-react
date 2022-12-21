7cef567a215ce66501adb7113df0480dabc5e955
Ewa Paszkowska <ewqa89@gmail.com>
2022-12-21 21:46:01 +0100

    transfer currency converter do react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "./Container";
 
 
 function App() {
+
   return (
     <Container>
       <Form />
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,44 +1,115 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form js-form">
+const currencys = [
+    {
+        name: "Euro",
+        short: "EUR",
+        course: 4.6648
+    },
+    {
+        name: "Dolar amerykański",
+        short: "USD",
+        course: 4.3947
+    },
+    {
+        name: "Korona czeska",
+        short: "CZK",
+        course: 0.1927
+    },
+    {
+        name: "Korona norweska",
+        short: "NOK",
+        course: 0.4456
+    },
+];
+
+const Form = () => {
+
+    const [amount, setAmount] = useState("");
+
+    const [currency, setCurrency] = useState(currencys[0].short);
+
+    const [result, setResult] = useState(null);
+
+    const calculate = (amount, currency) => {
+
+        const { course, short } = currencys.find(({ short }) => short === currency);
+        setResult((amount / course).toFixed(2) + ' ' + short);
+    };
+
+    const foundCourse = (currency) => {
+        const actualSetValue = currencys.find(element => element.short === currency)
+        return actualSetValue.course;
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculate(amount, currency);
+    };
+
+    return (
+        <form
+            className="form"
+            onSubmit={onFormSubmit}
+        >
             <fieldset className="form__fildset">
                 <legend className="form__legend">
                     Kalkulator walut
                 </legend>
                 <p>
                     <label>
-                        <span className="form__labelText">Kwota jaką chcesz wymienić:</span>
-                        <input className="form__input js-value" type="number" name="value" step="any" min="0" autofocus />
+                        <span className="form__labelText">
+                            Kwota PLN jaką chcesz wymienić:
+                        </span>
+                        <input
+                            className="form__input"
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            type="number"
+                            step="any"
+                            min="0"
+                            autofocus />
                     </label>
                 </p>
                 <p>
                     <label>
                         <span className="form__labelText">Wybierz walutę:</span>
-                        <select className="form__select js-currency" name="currency">
-                            <option value="4.7804">EUR</option>
-                            <option value="4.9000">USD</option>
-                            <option value="3.2463">JPY</option>
-                            <option value="4.8520">CHF</option>
+
+                        <select
+                            className="form__select"
+                            name="currency"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencys.map((currency) => (
+                                <option
+                                    key={currency.short}
+                                    value={currency.short}>
+                                    {currency.name}
+                                </option>
+                            ))};
                         </select>
-                        <p>
-                            <span className="js-exchangeChoose">0</span>
-                        </p>
+                        <span> *Kurs: {foundCourse(currency)}
+                        </span>
                     </label>
                 </p>
-            </fieldset>
-            <p>
-                <button className="form__button">Przelicz</button>
-            </p>
-            <fieldset className="form__fildset form__fildset--result">
+            </fieldset><p>
+                <button
+                    className="form__button"
+                >
+                    Przelicz
+                </button>
+            </p><fieldset className="form__fildset form__fildset--result">
                 <p>
                     <label className="form__label">
-                        Po przeliczeniu otrzymasz: <strong className="js-result"></strong> zł
+                        Po przeliczeniu otrzymasz: <strong>{result}</strong>
                     </label>
                 </p>
-                <p className="form__paragraph">*Kurs z dnia 22.10.2022</p>
+                <p className="form__paragraph">*Kurs z dnia 21.12.2022</p>
             </fieldset>
         </form>
-);
+    );
+};
 
 export default Form;
